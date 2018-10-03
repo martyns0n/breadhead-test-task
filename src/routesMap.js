36ed882b432c99b93a5eb92bd158f796fe797bb3
Volodya Martynson <zogacc@gmail.com>
2018-10-03 17:02:32 +0300

    add getting JSON data from server

--- src/routesMap.js
+++ src/routesMap.js
@@ -1,8 +1,18 @@
+import axios from 'axios';
+
 export default {
-  HOME: {
-    path: '/',
-  },
-  DASHBOARD: {
+	HOME: {
+		path: '/',
+		thunk: homeThunk
+	},
+	DASHBOARD: {
     path: '/dashboard',
-  },
+    thunk: homeThunk
+	}
+};
+
+const homeThunk = async (dispatch, getState) => {
+  // console.log('im called')
+  const { data } = await axios.get('http://localhost:8080')
+  console.log('⭐', data)
 };
