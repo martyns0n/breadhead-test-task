b7f77fa1b002fc6fb17ef85400bbacba88d4a2ac
Volodya Martynson <zogacc@gmail.com>
2018-10-04 14:23:24 +0300

    refactor component

--- src/components/screens/Home/Home.js
+++ src/components/screens/Home/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 import axios from 'axios'
 import Link from 'redux-first-router-link';
 import { forEach } from 'async';
-
+import styles from './index.css';
 
 class Home extends React.Component {
   constructor() {
@@ -25,34 +25,37 @@ class Home extends React.Component {
       component.setState({
         offers: response.data
       });
-      // console.log('⭐', this.state.offers);
     })
     .catch(console.log);
-    // this.state.offers.map((element) => {
-    //   console.log(element)
-    // });
   }
 
-  componentWillUpdate() {
-
-  }
-  
   render() {
     return (
-      <div className="offers">
-        <div className="name">
-          { this.state.offers.offer1.id || '' }
-        </div>
-        <div className="old-price"> 
-        { this.state.offers.offer1.name || '' }
-        </div>
-        <div className="new-price">
-        { this.state.offers.offer1.oldPrice || '' }
-        </div>
-        <div className="sizes">
-        { this.state.offers.offer1.newPrice || '' }
-        </div>
-      </div>
+      <React.Fragment>
+        <h1>Кроссовки на скидках</h1>
+        <ul className={styles.offers}>
+          <li className={styles.offer}>
+            <h2 className="">
+              { this.state.offers.offer1.name }
+            </h2>
+            <h3 className="">
+              { this.state.offers.offer1.store }
+            </h3>
+            <div className="">
+            { this.state.offers.offer1.oldPrice }
+            </div>
+            <div className="">
+            { this.state.offers.offer1.newPrice }
+            </div>
+            <div className=""> 
+            { this.state.offers.offer1.sizeType }
+            </div>
+            <div className=""> 
+            { this.state.offers.offer1.sizes }
+            </div>
+          </li>
+        </ul>
+      </React.Fragment>
     )
   }
 }
