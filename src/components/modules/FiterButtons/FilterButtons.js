54eafba8f842ccd2c38befa9f495cb46076f02fe
Volodya Martynson <zogacc@gmail.com>
2018-10-14 15:27:10 +0300

    replace class with arrow function

--- src/components/modules/FiterButtons/FilterButtons.js
+++ src/components/modules/FiterButtons/FilterButtons.js
@@ -2,33 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.css';
 
-class FilterButtons extends React.Component {
-  render() {
-    return (
-      <div className={styles.filter}>
-        <h2 className={styles.subtitle}>Фильтр размеров</h2>
-        {
-          this.props.sizes.map(size => (
-            <button
-              className={styles.toggleSizeButton}
-              key={size}
-              onClick={(event) => {
-                event.currentTarget.classList.toggle(styles.active);
-                this.props.toggleFilter(size);
-              }}
-            >
-              {size}
-            </button>
-          ))
-        }
-      </div>
-    );
-  }
-}
+const FilterButtons = props => (
+  <div className={styles.filter}>
+    <h2 className={styles.subtitle}>Фильтр размеров</h2>
+    {
+      props.sizes.map(size => (
+        <button
+          className={styles.toggleSizeButton}
+          key={size}
+          onClick={(event) => {
+            event.currentTarget.classList.toggle(styles.active);
+            props.toggleFilter(size);
+          }}
+        >
+          {size}
+        </button>
+      ))
+    }
+  </div>
+);
 
 FilterButtons.propTypes = {
   sizes: PropTypes.arrayOf(PropTypes.string).isRequired,
-  // filters: PropTypes.arrayOf(PropTypes.string).isRequired,
   toggleFilter: PropTypes.func.isRequired,
 };
 
